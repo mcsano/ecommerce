51632ba5055af1df78100b341cb90fd5f512dc2b
mcsano <128045974+mcsano@users.noreply.github.com>
2023-03-18 20:02:23 +0800

    Add files via upload

---
+++ midterm/order module/order_placed.php
@@ -0,0 +1,15 @@
+<!-- shop.php -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Shop</title>
+</head>
+<body>
+	<h1>ORDER PLACED!</h1>
+	<form action="view_order.php" method="post">
+	<input type="submit" value="View Order">
+	</form>
+
+</body>
+</html>
---
+++ midterm/users.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 18, 2023 at 05:55 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `echo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `contact_num` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `name`, `address`, `email`, `contact_num`) VALUES
+(4, 'Chester Saño', 'Oas, Albay', 'chester@gmail.com', 2147483647),
+(5, 'Mary Cris Bayola', 'Pinit, Ligao City', 'marycris@gmail.com', 2147483647),
+(6, 'Angelica Sabado', 'Ligao City', 'sabadoangelica@gmail.com', 2147483647),
+(7, 'Rico Doctor', 'Ligao City', 'ricodoctors@gmail.com', 2147483647);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
